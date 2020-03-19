5ce9b4d16d2c5769e9338427b77e874b05e4af3c
postables <postables@rtradetechnologies.com>
2020-03-18 22:59:38 -0700

    chore: start integrating lens

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/RTradeLtd/iprfc
 
 go 1.14
+
+require (
+	github.com/RTradeLtd/config v1.1.2
+	github.com/RTradeLtd/grpc v1.4.0
+	google.golang.org/grpc v1.28.0
+)
---
+++ lens/lens.go
@@ -0,0 +1,51 @@
+package lens
+
+import (
+	"fmt"
+
+	"github.com/RTradeLtd/config"
+	"github.com/RTradeLtd/grpc/dialer"
+	pb "github.com/RTradeLtd/grpc/lens"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+const (
+	defaultURL = "0.0.0.0:9998"
+)
+
+// Client is a lens client used to make requests to the Lens gRPC server
+type Client struct {
+	pb.IndexerAPIClient
+}
+
+// NewClient is used to generate our lens client
+func NewClient(opts config.Endpoints) (*Client, error) {
+	dialOpts := make([]grpc.DialOption, 0)
+	// setup parameters for our conection to
+	if opts.Lens.TLS.CertPath != "" {
+		fmt.Println("using secure connection")
+		creds, err := credentials.NewClientTLSFromFile(opts.Lens.TLS.CertPath, "")
+		if err != nil {
+			return nil, fmt.Errorf("could not load tls cert: %s", err)
+		}
+		dialOpts = append(dialOpts,
+			grpc.WithTransportCredentials(creds),
+			grpc.WithPerRPCCredentials(dialer.NewCredentials(opts.Lens.AuthKey, false)))
+	} else {
+		fmt.Println("using insecure connection")
+		dialOpts = append(dialOpts, grpc.WithInsecure())
+	}
+	var url string
+	if opts.Lens.URL == "" {
+		url = defaultURL
+	}
+	fmt.Println(url)
+	gConn, err := grpc.Dial(url, dialOpts...)
+	if err != nil {
+		return nil, err
+	}
+	client := &Client{}
+	client.IndexerAPIClient = pb.NewIndexerAPIClient(gConn)
+	return client, nil
+}
